83168239e670732595be849a861a549e80133efc
3122 <13507191640yt>
2020-10-14 00:34:23 +0800

    整理pom文件中不必要的依赖

--- src/main/java/com/microtao/entity/User.java
+++ src/main/java/com/microtao/entity/User.java
@@ -1,28 +1,15 @@
 package com.microtao.entity;
 
+import lombok.Data;
+
 /**
  * 用户实体类
  *
  * @Author 於涛
  * @Date 2020/10/12-18:20
  */
+@Data
 public class User {
     private String userName;
     private int age;
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
